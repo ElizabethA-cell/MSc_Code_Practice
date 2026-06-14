4bd4b573924cc2db0fd958b85f7e24404828b1d5
ElizabethA-cell <sgealam@liverpool.ac.uk>
2026-06-14 14:29:28 +0800

    Add PEP 8 rewrites for HolyPython Lesson 18 examples

---
+++ lesson18_operators_pep8.py
@@ -0,0 +1,35 @@
+# Example 1a
+greek_island = "santorini"
+
+# Example 1b
+greek_island += " is beautiful"
+
+# Example 1c
+greek_island *= 3
+
+
+# Example 2a
+x = 5
+y = 3
+print(x + y)
+
+# Example 2b
+x = 5
+y = 3
+print(x * y)
+
+
+# Example 3a
+my_list = [1, 2, 3]
+my_list += [4, 5]
+print(my_list)
+
+# Example 3b
+my_list *= 2
+print(my_list)
+
+
+# Example 4
+a = 10
+b = 3
+print(a % b)
